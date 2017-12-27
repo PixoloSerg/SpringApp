9e1da804fed94873ea1f6ddaac7e15ed443b154b
Sveta <Sveta@PIXOLO>
2017-12-27 19:03:53 +0300

    Add welcome message.

--- SpringApp/src/springapp/SpringApp.java
+++ SpringApp/src/springapp/SpringApp.java
@@ -15,7 +15,7 @@ public class SpringApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-	// TODO code application logic here
+	System.out.println("Message from branch1");
     }
     
 }
